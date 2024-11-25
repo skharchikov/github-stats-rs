86ba0dfee069d6f88ff2be797256e3ec38abfc0e
Sergei Kharchikov <callmestech@gmail.com>
2024-11-25 23:33:05 +0000

    pagination

--- src/service/github.rs
+++ src/service/github.rs
@@ -95,56 +95,107 @@ impl GithubExt for Github {
     }
 
     fn get_stats(&self) -> Result<Stats, anyhow::Error> {
-        let mut next_owned = "";
-        let mut next_contrib = "";
+        let mut next_owned = None;
+        let mut next_contrib = None;
 
+        let mut name = None;
         let mut repos: Vec<String> = vec![];
         let mut forks = 0;
         let mut stargazers = 0;
+        let mut languages: HashMap<String, Language> = HashMap::new();
 
-        let variables = repos_overview::Variables {
-            owned_cursor: None,
-            contributed_cursor: None,
-        };
-        let raw_results = post_graphql_blocking::<ReposOverview, _>(
-            &self.client,
-            &self.graphql_url(),
-            variables,
-        )?;
+        loop {
+            let variables = repos_overview::Variables {
+                owned_cursor: next_owned,
+                contributed_cursor: next_contrib,
+            };
+            let raw_results = post_graphql_blocking::<ReposOverview, _>(
+                &self.client,
+                &self.graphql_url(),
+                variables,
+            )?;
 
-        let name = raw_results
-            .data
-            .as_ref()
-            .and_then(|data| data.viewer.name.clone())
-            .unwrap_or("No Name".to_string());
-
-        let mut languages_contributed = HashMap::new();
-
-        if self.configuration.exclude_forked_repos() {
-            // do nothing
-        } else {
-            let contributed_repos = raw_results
+            name = name.or(raw_results
                 .data
                 .as_ref()
-                .and_then(|data| data.viewer.repositories_contributed_to.nodes.as_ref())
-                .map(|nodes| {
-                    nodes
-                        .iter()
-                        .filter(|opt| opt.is_some())
-                        .flatten()
-                        .collect::<Vec<_>>()
-                });
+                .and_then(|data| data.viewer.name.clone()));
 
-            for repo in contributed_repos.iter().flatten() {
-                repos.push(repo.name_with_owner.clone());
-                forks += repo.fork_count;
-                stargazers += repo.stargazers.total_count;
+            let mut languages_contributed = languages;
+
+            if self.configuration.exclude_forked_repos() {
+                // do nothing
+            } else {
+                let contributed_repos = raw_results
+                    .data
+                    .as_ref()
+                    .and_then(|data| data.viewer.repositories_contributed_to.nodes.as_ref())
+                    .map(|nodes| {
+                        nodes
+                            .iter()
+                            .filter(|opt| opt.is_some())
+                            .flatten()
+                            .collect::<Vec<_>>()
+                    });
+
+                for repo in contributed_repos.iter().flatten() {
+                    repos.push(repo.name_with_owner.clone());
+                    forks += repo.fork_count;
+                    stargazers += repo.stargazers.total_count;
+                }
+
+                languages_contributed = contributed_repos
+                    .iter()
+                    .flatten()
+                    .filter_map(|repo| repo.languages.as_ref())
+                    .filter_map(|languages| languages.edges.as_ref())
+                    .flatten()
+                    .flatten()
+                    .map(|edge| {
+                        Language::new(
+                            edge.node.name.clone(),
+                            edge.size,
+                            1,
+                            edge.node.color.clone().unwrap_or("#000000".to_string()),
+                            0.0,
+                        )
+                    })
+                    .collect::<Vec<_>>()
+                    .iter()
+                    .fold(HashMap::new(), |mut acc, lang| {
+                        acc.entry(lang.name().to_string())
+                            .and_modify(|e: &mut Language| {
+                                let new_zise: i64 = e.size() + lang.size();
+                                let new_occurences: i64 = e.occurrences() + lang.occurrences();
+                                e.set_occurrences(new_occurences);
+                                e.set_size(new_zise);
+                            })
+                            .or_insert(lang.clone());
+                        acc
+                    });
             }
 
-            languages_contributed = contributed_repos
+            let owned_repos = raw_results
+                .data
+                .as_ref()
+                .map(|data| &data.viewer.repositories);
+
+            for repo in owned_repos
                 .iter()
+                .flat_map(|repos| &repos.nodes)
                 .flatten()
-                .filter_map(|repo| repo.languages.as_ref())
+                .flatten()
+            {
+                repos.push(repo.name_with_owner.clone());
+                forks += repo.fork_count;
+                stargazers += repo.stargazer_count;
+            }
+
+            languages = owned_repos
+                .iter()
+                .flat_map(|repos| &repos.nodes)
+                .flatten()
+                .flatten()
+                .filter_map(|nodes| nodes.languages.as_ref())
                 .filter_map(|languages| languages.edges.as_ref())
                 .flatten()
                 .flatten()
@@ -159,7 +210,7 @@ impl GithubExt for Github {
                 })
                 .collect::<Vec<_>>()
                 .iter()
-                .fold(HashMap::new(), |mut acc, lang| {
+                .fold(languages_contributed, |mut acc, lang| {
                     acc.entry(lang.name().to_string())
                         .and_modify(|e: &mut Language| {
                             let new_zise: i64 = e.size() + lang.size();
@@ -170,66 +221,50 @@ impl GithubExt for Github {
                         .or_insert(lang.clone());
                     acc
                 });
-        }
-
-        let owned_repos = raw_results
-            .data
-            .as_ref()
-            .map(|data| &data.viewer.repositories);
-
-        for repo in owned_repos
-            .iter()
-            .flat_map(|repos| &repos.nodes)
-            .flatten()
-            .flatten()
-        {
-            repos.push(repo.name_with_owner.clone());
-            forks += repo.fork_count;
-            stargazers += repo.stargazer_count;
-        }
-
-        let mut languages = owned_repos
-            .iter()
-            .flat_map(|repos| &repos.nodes)
-            .flatten()
-            .flatten()
-            .filter_map(|nodes| nodes.languages.as_ref())
-            .filter_map(|languages| languages.edges.as_ref())
-            .flatten()
-            .flatten()
-            .map(|edge| {
-                Language::new(
-                    edge.node.name.clone(),
-                    edge.size,
-                    1,
-                    edge.node.color.clone().unwrap_or("#000000".to_string()),
-                    0.0,
-                )
-            })
-            .collect::<Vec<_>>()
-            .iter()
-            .fold(languages_contributed, |mut acc, lang| {
-                acc.entry(lang.name().to_string())
-                    .and_modify(|e: &mut Language| {
-                        let new_zise: i64 = e.size() + lang.size();
-                        let new_occurences: i64 = e.occurrences() + lang.occurrences();
-                        e.set_occurrences(new_occurences);
-                        e.set_size(new_zise);
-                    })
-                    .or_insert(lang.clone());
-                acc
+            let total_size = languages.values().map(|lang| lang.size()).sum::<i64>();
+            languages.iter_mut().for_each(|(_, lang)| {
+                lang.set_proportion(100f64 * lang.size() as f64 / total_size as f64);
             });
-        let total_size = languages.values().map(|lang| lang.size()).sum::<i64>();
-        languages.iter_mut().for_each(|(_, lang)| {
-            lang.set_proportion(100f64 * lang.size() as f64 / total_size as f64);
-        });
 
+            let has_next_owned = owned_repos
+                .as_ref()
+                .map(|repos| repos.page_info.has_next_page)
+                .unwrap_or_default();
+
+            let has_next_contrib = !self.configuration.exclude_forked_repos()
+                && raw_results
+                    .data
+                    .as_ref()
+                    .map(|data| {
+                        data.viewer
+                            .repositories_contributed_to
+                            .page_info
+                            .has_next_page
+                    })
+                    .unwrap_or_default();
+
+            if has_next_owned || has_next_contrib {
+                next_owned = owned_repos
+                    .as_ref()
+                    .and_then(|repos| repos.page_info.end_cursor.as_ref().cloned());
+                next_contrib = raw_results.data.as_ref().and_then(|data| {
+                    data.viewer
+                        .repositories_contributed_to
+                        .page_info
+                        .end_cursor
+                        .as_ref()
+                        .cloned()
+                });
+            } else {
+                break;
+            }
+        }
         let total_contributions = self.total_contributions()?;
         let views = self.views(&repos)?;
         let lines_changed = self.lines_changed(&repos)?;
 
         Ok(Stats::builder()
-            .name(name)
+            .name(name.unwrap_or_default())
             .total_contributions(total_contributions)
             .views(views)
             .lines_changed(lines_changed)
